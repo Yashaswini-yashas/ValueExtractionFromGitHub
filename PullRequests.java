b598d7028cf5798f42eecbcae9845f813c234eec
YashaswiniS1234 <yashaswini.a.s@capgemini.com>
2020-04-10 14:29:21 +0530

    gives the pull requests for the repository

---
+++ PullRequests.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import javax.xml.ws.Response;
+
+import io.restassured.path.json.JsonPath;
+
+public class PullRequests {
+	
+public static void main(String args[]) {
+	String username = "Yashaswini-yashas";
+	String password = "Pe1@Ac2H";
+	String url = "https://api.github.com/repos/Yashaswini-yashas/petclinic/pulls";
+	
+	String[] command = {"curl", "-u", username+":"+password , url};
+	 ProcessBuilder process = new ProcessBuilder(command); 
+     Process p;
+     try
+     {
+         p = process.start();
+          BufferedReader reader =  new BufferedReader(new InputStreamReader(p.getInputStream()));
+             StringBuilder builder = new StringBuilder();
+             String line = null;
+             while ( (line = reader.readLine()) != null) {
+                     builder.append(line);
+                     builder.append(System.getProperty("line.separator"));
+             }
+             String result = builder.toString();
+             System.out.print(result); 
+     }      
+     catch (IOException e)
+     {   System.out.print("error");
+         e.printStackTrace();
+     }
+     
+}
+}
